ad99eae3dda3cac8c5d0c997068908abe74e10b8
hi08060204 <hi08060204@gmail.com>
2014-03-18 23:51:37 +0800

    Added the function for drawing tab

--- chordEdit/static/chordEdit/submitURL.js
+++ chordEdit/static/chordEdit/submitURL.js
@@ -20,6 +20,9 @@ $('#submit').click(function() {
             document.getElementById("submit").innerHTML = " Submit";      
             console.log(data);
             player.loadVideoById(data);
+            document.getElementById("tab").innerHTML = DrawingTab(8,4,4);
+            $('#tab').hide(500);
+            $("#tab").show("slow");
         } 
         
     });
@@ -38,4 +41,21 @@ function onPlayerReady(event) {
                 ss = (ss < 10) ? ('0' + String(ss)) : String(ss);
                 clock.innerHTML = mm + ':' + ss;
         }, 100); 
+}
+
+
+function DrawingTab(numRows, beatsPerMeasure, measuresPerRow) { 
+    var context = ""; 
+    for (var i=0;i < numRows;i++){
+        context  = context + "<div class='row-fluid grid-show'>";
+        for (var j=0;j < measuresPerRow; j++ ){
+            context = context + "<div class='span3'><div class='row-fluid grid-show'>";
+            for (var k=1;k <= beatsPerMeasure; k++)
+                context = context + "<div class='span3'>" + k.toString() + "</div>";
+        
+            context = context + "</div></div>";
+        }
+        context = context + "</div>"
+    }   
+    return context;
 }
